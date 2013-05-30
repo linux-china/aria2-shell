baf281aa6ebb384576b7e212ffecbe4dd4519f88
Jacky Chan <libing.chen@gmail.com>
2013-05-30 13:00:23 +0800

    add display gid fields

--- src/test/java/org/mvnsearch/aria2/service/impl/AriaServiceImplTest.java
+++ src/test/java/org/mvnsearch/aria2/service/impl/AriaServiceImplTest.java
@@ -3,6 +3,7 @@ package org.mvnsearch.aria2.service.impl;
 import junit.framework.TestCase;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -35,12 +36,15 @@ public class AriaServiceImplTest extends TestCase {
      */
     public void testAddUri() throws Exception {
         String uri = "http://cdntest.aliyun.com/faxianla/metal/m937309-1366561245640.jpg";
-        String gid = ariaService.addUri(uri, Collections.emptyMap());
+        Map<Object, Object> params = new HashMap<Object, Object>();
+//      params.put("gid", "181b1205c1da6370");
+        params.put("gid", "0000000000000001");
+        String gid = ariaService.addUri(uri, params);
         System.out.println(gid);
     }
 
     /**
-     * test to tell status
+     * test to info status
      *
      * @throws Exception exception
      */
@@ -50,12 +54,20 @@ public class AriaServiceImplTest extends TestCase {
     }
 
     /**
-     * test to tell stopped
+     * test to info stopped
      *
      * @throws Exception exception
      */
     public void testTellStopped() throws Exception {
         List<Map<String, Object>> queue = ariaService.tellStopped(0, 10);
-        System.out.println(queue.size());
+        for (Map<String, Object> task : queue) {
+            String gid = (String) task.get("gid");
+            String status = (String) task.get("status");
+            Map<String, Object> files = (Map<String, Object>) ((Object[]) task.get("files"))[0];
+            String path = (String) files.get("path");
+            Map<String, Object> uriInfo = (Map<String, Object>) ((Object[]) files.get("uris"))[0];
+            String uri = (String) uriInfo.get("uri");
+            System.out.println(task);
+        }
     }
 }
