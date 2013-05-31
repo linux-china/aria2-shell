7c4f540cfe3e5654fd9c7296c6a7d184c9b8d45d
Jacky Chan <libing.chen@gmail.com>
2013-05-31 18:15:53 +0800

    adjust api

--- src/main/java/org/mvnsearch/aria2/service/impl/AriaServiceImpl.java
+++ src/main/java/org/mvnsearch/aria2/service/impl/AriaServiceImpl.java
@@ -2,7 +2,9 @@ package org.mvnsearch.aria2.service.impl;
 
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+import org.mvnsearch.aria2.service.Aria2Ops;
 import org.mvnsearch.aria2.service.AriaService;
+import org.mvnsearch.aria2.xmlrpc.XmlRpcFactoryBean;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
@@ -17,10 +19,6 @@ import java.util.*;
 @Component("ariaService")
 public class AriaServiceImpl implements AriaService {
     /**
-     * xml-rpc client
-     */
-    private XmlRpcClient client;
-    /**
      * xml rpc host
      */
     private String host;
@@ -29,9 +27,9 @@ public class AriaServiceImpl implements AriaService {
      */
     private int port;
     /**
-     * empty params
+     * aria2 ops
      */
-    private static List<Object> emptyParams = Collections.emptyList();
+    private Aria2Ops aria2Ops;
 
     /**
      * connect with host
@@ -43,10 +41,12 @@ public class AriaServiceImpl implements AriaService {
         this.host = host;
         this.port = port;
         String xmlRpcUrl = "http://" + host + ":" + port + "/rpc";
-        XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
-        config.setServerURL(new URL(xmlRpcUrl));
-        client = new XmlRpcClient();
-        client.setConfig(config);
+        XmlRpcFactoryBean factoryBean = new XmlRpcFactoryBean();
+        factoryBean.setMethodPrefix("aria2.");
+        factoryBean.setServiceUrl(xmlRpcUrl);
+        factoryBean.setServiceInterface(Aria2Ops.class);
+        factoryBean.afterPropertiesSet();
+        this.aria2Ops = (Aria2Ops) factoryBean.getObject();
     }
 
     /**
@@ -77,197 +77,11 @@ public class AriaServiceImpl implements AriaService {
     }
 
     /**
-     * download url with options
+     * get aria2 ops
      *
-     * @param url     url
-     * @param options options
-     * @return gid
+     * @return ops
      */
-    public String addUri(String url, Map<Object, Object> options) throws Exception {
-        List<Object> params = constructParams(new Object[]{url}, options);
-        return (String) client.execute("aria2.addUri", params);
+    public Aria2Ops getAria2Ops() {
+        return this.aria2Ops;
     }
-
-    /**
-     * remove download
-     *
-     * @param gid gid
-     * @throws Exception exception
-     */
-    public void remove(String gid) throws Exception {
-        List<Object> params = constructParams(gid);
-        client.execute("aria2.remove", params);
-    }
-
-    /**
-     * pause download
-     *
-     * @param gid gid
-     * @throws Exception exception
-     */
-    public void pause(String gid) throws Exception {
-        List<Object> params = constructParams(gid);
-        client.execute("aria2.pause", params);
-    }
-
-    /**
-     * unpause gid
-     *
-     * @param gid gid
-     * @throws Exception exception
-     */
-    public void unpause(String gid) throws Exception {
-        List<Object> params = constructParams(gid);
-        client.execute("aria2.unpause", params);
-    }
-
-    /**
-     * info download status
-     *
-     * @param gid gid
-     * @return status information
-     */
-    public Map<String, Object> tellStatus(String gid) throws Exception {
-        List<Object> params = constructParams(gid);
-        return (Map<String, Object>) client.execute("aria2.tellStatus", params);
-    }
-
-    /**
-     * info waiting
-     *
-     * @param offset     offset
-     * @param maxResults max results
-     * @return gid list
-     */
-    public List<Map<String, Object>> tellWaiting(int offset, int maxResults) throws Exception {
-        List<Object> params = constructParams(offset, maxResults);
-        Object[] results = (Object[]) client.execute("aria2.tellWaiting", params);
-        return convertToList(results);
-    }
-
-    /**
-     * info active
-     *
-     * @return active list
-     * @throws Exception exception
-     */
-    public List<Map<String, Object>> tellActive() throws Exception {
-        Object[] results = (Object[]) client.execute("aria2.tellActive", emptyParams);
-        return convertToList(results);
-    }
-
-    /**
-     * info stopped
-     *
-     * @param offset     offset
-     * @param maxResults max results
-     * @return stopped list
-     * @throws Exception exception
-     */
-    public List<Map<String, Object>> tellStopped(int offset, int maxResults) throws Exception {
-        List<Object> params = constructParams(offset, maxResults);
-        Object[] results = (Object[]) client.execute("aria2.tellStopped", params);
-        return convertToList(results);
-    }
-
-    /**
-     * convert to list
-     *
-     * @param results results
-     * @return item list
-     */
-    private List<Map<String, Object>> convertToList(Object[] results) {
-        if (results != null && results.length > 0) {
-            List<Map<String, Object>> items = new ArrayList<Map<String, Object>>();
-            for (Object result : results) {
-                items.add((Map<String, Object>) result);
-            }
-            return items;
-        }
-        return Collections.emptyList();
-    }
-
-    /**
-     * get global stat
-     * <pre>
-     * numActive:0
-     * downloadSpeed:0
-     * uploadSpeed:0
-     * numWaiting:0
-     * numStopped:2
-     * </pre>
-     *
-     * @return global stat
-     */
-    public Map<String, String> getGlobalStat() throws Exception {
-        return (Map<String, String>) client.execute("aria2.getGlobalStat", emptyParams);
-    }
-
-    /**
-     * Pause all the active downloads
-     */
-    public void sleep() throws Exception {
-        client.execute("aria2.pauseAll", emptyParams);
-    }
-
-    /**
-     * Resume all the paused downloads
-     */
-    public void wake() throws Exception {
-        client.execute("aria2.unpauseAll", emptyParams);
-    }
-
-    /**
-     * Clear the list of stopped downloads and errors
-     */
-    public void purge() throws Exception {
-        client.execute("aria2.purgeDownloadResult", emptyParams);
-    }
-
-    /**
-     * get version information
-     *
-     * @return version information
-     * @throws Exception exception
-     */
-    public Map<String, Object> version() throws Exception {
-        return (Map<String, Object>) client.execute("aria2.getVersion", emptyParams);
-    }
-
-    /**
-     * get global option
-     *
-     * @return global option
-     * @throws Exception exception
-     */
-    public Map<String, Object> getGlobalOption() throws Exception {
-        return (Map<String, Object>) client.execute("aria2.getGlobalOption", emptyParams);
-    }
-
-    /**
-     * change global option
-     *
-     * @param name  name
-     * @param value value
-     * @throws Exception exception
-     */
-    public void changeGlobalOption(String name, Object value) throws Exception {
-        Map<String, Object> options = new HashMap<String, Object>();
-        options.put(name, value);
-        List<Object> params = constructParams(options);
-        client.execute("aria2.changeGlobalOption", params);
-    }
-
-    /**
-     * construct params
-     *
-     * @param params params
-     * @return list object
-     */
-    public List<Object> constructParams(Object... params) {
-        List<Object> temp = new ArrayList<Object>();
-        Collections.addAll(temp, params);
-        return temp;
-    }
-
 }
